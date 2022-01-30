54ec8d95e75f8a21ebb43defe14cf1106f9ed8d5
Chris Wray <chris@solmediaco.com>
2022-01-29 16:52:11 -0800

    create controller methods for imports + add import job

--- server/app/controllers/api/prospect_files_controller.rb
+++ server/app/controllers/api/prospect_files_controller.rb
@@ -1,2 +1,20 @@
 class ProspectFilesController < ApplicationController
+  def import
+    # Create a new ProspectFile
+    prospect_file = ProspectFile.new
+    prospect_file.file = params[:file]
+    prospect_file.save
+
+    # Send the file to the import job
+    ImportProspectsJob.perform_later(prospect_file.id)
+
+    # Return the status of the import job
+    render json: { status: prospect_file.status }
+  end
+
+  def progress
+    prospect_file = ProspectFile.find(params[:id])
+
+    render json: { total: prospect_file.total, done: prospect_file.done }
+  end
 end
