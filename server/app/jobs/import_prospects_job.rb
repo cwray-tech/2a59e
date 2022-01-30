4906579e817d099a42e280d75de402c208a6af14
Chris Wray <chris@solmediaco.com>
2022-01-29 17:01:33 -0800

    add job method

--- server/app/jobs/import_prospects_job.rb
+++ server/app/jobs/import_prospects_job.rb
@@ -2,6 +2,20 @@ class ImportProspectsJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    # Do something later
+    # Find the prospect file
+    prospect_file = ProspectFile.find(args[0])
+
+    # Import the prospects
+    prospect_file.import_prospects
+
+    # Update the status of the prospect file
+    prospect_file.update_status
+
+    # Send the status of the import job
+    ActionCable.server.broadcast "prospect_files_channel",
+      status: prospect_file.status
+
+    # Return the status of the import job
+    render json: { status: prospect_file.status }
   end
 end
--- server/app/models/prospect_file.rb
+++ server/app/models/prospect_file.rb
@@ -1,3 +1,16 @@
 class ProspectFile < ApplicationRecord
   has_one_attached :file
+
+  validates :file, presence: true
+
+  enum status: {
+    pending: 0,
+    processing: 1,
+    done: 2,
+    error: 3
+  }
+
+  def import_prospects 
+    
+  end
 end
