c61995eb6c206205fd6c019f0b245963b961a916
Chris Wray <chris@solmediaco.com>
2022-01-31 09:57:48 -0800

    remove status from prospect file model

--- server/app/jobs/import_prospects_job.rb
+++ server/app/jobs/import_prospects_job.rb
@@ -5,17 +5,6 @@ class ImportProspectsJob < ApplicationJob
     # Find the prospect file
     prospect_file = ProspectFile.find(args[0])
 
-    # Import the prospects
     prospect_file.import_prospects
-
-    # Update the status of the prospect file
-    prospect_file.update_status
-
-    # Send the status of the import job
-    ActionCable.server.broadcast "prospect_files_channel",
-      status: prospect_file.status
-
-    # Return the status of the import job
-    render json: { status: prospect_file.status }
   end
 end
--- server/app/models/prospect_file.rb
+++ server/app/models/prospect_file.rb
@@ -3,13 +3,6 @@ class ProspectFile < ApplicationRecord
 
   validates :file, presence: true
 
-  enum status: {
-    pending: 0,
-    processing: 1,
-    done: 2,
-    error: 3
-  }
-
   def import_prospects 
     
   end
